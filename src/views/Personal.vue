1ede44d8e1ef139c4da6f5e8ccd0d7959d72b862
nicolewang2004 <109955987+nicolewang2004@users.noreply.github.com>
2023-07-31 10:57:06 +0800

    feat:补全状态栏，并新增个人信息页面

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -5,25 +5,60 @@
 
   <!--这里是页面的下半部分-->
   <div class="background">
-    <!--上半部分用户以及下方的切换栏-->
-    <div class="up-content">
-      <!--上半部分的人物部分-->
-      <div class="user-content">
+    <div class="main-content">
+        <!--上半部分用户以及下方的切换栏-->
+        <div class="up-content">
+            <!--上半部分的人物部分-->
+            <div class="user-content">
 
-        <!--用户头像-->
-        <img class="user-head-image" src="../assets/default_account.svg"/>
+                <!--用户头像-->
+                <img class="user-head-image" src="../assets/default_account.svg"/>
 
-        <!--用户姓名-->
-        <div class="user-name-text">用户1234</div>
+                <!--用户姓名-->
+                <div class="user-name-text">用户1234</div>
 
-        <!--vip等级-->
-        <div class="vip-grade">&ensp;{{"vip"+vip_grade}}&ensp;</div>
-      </div>
+                <!--vip等级-->
+                <div class="vip-grade">&ensp;{{"vip"+vip_grade}}&ensp;</div>
+            </div>
 
-      <!--上部区的切换栏部分-->
-      <div class="borad">
-          
-      </div>
+            <!--上部区的切换栏部分-->
+            <div class="borad">
+                <!--页面切换-->
+                <div
+                        class="choose"
+                        v-for="item in opt"
+                        :key="item.id"
+                        :class="{ choose_active: chooseId === item.id }"
+                        @click="handleItemClick(item.id)"
+                >
+                    {{item.name}}
+                </div>
+                <!--特别人数-->
+                <div class="special-user">
+                    <!--关注数-->
+                    <div class="special-user-board">
+                        <img class="special-user-icon" src="../assets/personal/focus-people.png"/>
+                        <div class="special-user-text">
+                            <div class="amount-text">关注数</div>
+                            <div class="amount-num">{{focus_people}}</div>
+                        </div>
+                    </div>
+                    <!--粉丝数-->
+                    <div class="special-user-board">
+                        <img class="special-user-icon" src="../assets/personal/fan-people.png"/>
+                        <div class="special-user-text">
+                            <div class="amount-text">粉丝数</div>
+                            <div class="amount-num">{{fan_people}}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--个人页面-->
+        <div class="user-page">
+
+        </div>
     </div>
   </div>
 
@@ -40,7 +75,23 @@ export default {
   data(){
       return{
           vip_grade:1,
+          opt:[
+              {id:0,name:"主页"},
+              {id:1,name:"个人资料"},
+              {id:2,name:"收藏夹"},
+              {id:3,name:"设置"},
+          ],
+          chooseId:0,
+          focus_people:238,
+          fan_people:262,
       }
+  },
+  methods:{
+      handleItemClick(id) {
+          // 处理选项点击事件，切换选项的激活状态
+          this.chooseId = id;
+          // 在这里可以执行其他相应的操作，根据选项的 id 来处理不同的逻辑
+      },
   }
 }
 </script>
@@ -55,12 +106,20 @@ export default {
 
     background-color: #D9D9D9;
 }
+.main-content{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    top:80px;
+    width: 1480px;
+    height: 800px;
+}
 /* 上部区 */
 .up-content{
     position: relative;
     display: flex;
     flex-direction: column;
-    top:80px;
+    //top:80px;
     width: 1480px;
     height: 200px;
 }
@@ -118,6 +177,84 @@ export default {
     display: flex;
     width: 100%;
     height: 60px;
+    background-color: #FFFFFF;
+}
+.choose{
+    position: relative;
+    display: flex;
+    width: 150px;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    background-color: #FFFFFF;
+
+    color: #888;
+    font-family: MiSans;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+.choose_active{
+    background-color: #8097FD;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.05) inset;
+    color:#FFFFFF;
+}
+.special-user{
+    position: relative;
+    display: flex;
+    justify-content: flex-end;
+    width: 880px;
+    height: 100%;
+}
+.special-user-board{
+    position: relative;
+    display: flex;
+    width: 115px;
+    height: 52px;
+    margin-top: 8px;
+    margin-right: 45px;
+    align-items: center;
+}
+.special-user-icon{
+    position: relative;
+    width: 45px;
+    height: 45px;
+}
+.special-user-text{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 62px;
+    height: 50px;
+    margin-left: 4px;
+    justify-content: center;
+
+    color: #888;
+    font-family: MiSans;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+.amount-text{
+    position: relative;
+    width: 100%;
+    height: 24px;
+}
+.amount-num{
+    position: relative;
+    width: 100%;
+    height: 26px;
+}
+/* 个人资料部分 */
+.user-page{
+    position: relative;
+    display: flex;
+    width: 1480px;
+    height: 600px;
+    margin-top: 50px;
+
     background-color: #FFFFFF;
 }
 </style>
