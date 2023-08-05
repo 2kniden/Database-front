1d46da813bd81a9aaf6521a79cf06e5c99179fc5
coding-lele <3348545690@qq.com>
2023-08-05 18:10:37 +0800

    加入mock接口

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import './mock';
 
 import axios from "axios";
 import $ from "jquery";
