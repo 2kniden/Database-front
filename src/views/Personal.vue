bf0c181422fa50f3c43447850e2072ef700ba44a
nicolewang2004 <109955987+nicolewang2004@users.noreply.github.com>
2023-08-06 14:12:19 +0800

    把个人页面做的更加可爱美丽了，个人页面的首页制作完毕
    把个人页面的首页的布局进行了改变并添加了多个ui和功能键

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -5,61 +5,85 @@
 
   <!--这里是页面的下半部分-->
   <div class="background">
-    <div class="main-content">
-        <!--上半部分用户以及下方的切换栏-->
-        <div class="up-content">
-            <!--上半部分的人物部分-->
-            <div class="user-content">
-
-                <!--用户头像-->
-                <img class="user-head-image" src="../assets/default_account.svg"/>
-
-                <!--用户姓名-->
-                <div class="user-name-text">用户1234</div>
-
-                <!--vip等级-->
-                <div class="vip-grade">&ensp;{{"vip"+vip_grade}}&ensp;</div>
-            </div>
-
-            <!--上部区的切换栏部分-->
-            <div class="borad">
-                <!--页面切换-->
-                <div
-                        class="choose"
-                        v-for="item in opt"
-                        :key="item.id"
-                        :class="{ choose_active: chooseId === item.id }"
-                        @click="handleItemClick(item.id)"
-                >
-                    {{item.name}}
+      <div class="left_choose">
+        <div class="left_choose_change">
+            <div class="choose_content" @click="chooseinone">
+                <img class="choose_content_icon" src="../assets/personal/selfhome.png">
+                <div class="choose_content_text">
+                    个人中心
                 </div>
-                <!--特别人数-->
-                <div class="special-user">
-                    <!--关注数-->
-                    <div class="special-user-board">
-                        <img class="special-user-icon" src="../assets/personal/focus-people.png"/>
-                        <div class="special-user-text">
-                            <div class="amount-text">关注数</div>
-                            <div class="amount-num">{{focus_people}}</div>
-                        </div>
-                    </div>
-                    <!--粉丝数-->
-                    <div class="special-user-board">
-                        <img class="special-user-icon" src="../assets/personal/fan-people.png"/>
-                        <div class="special-user-text">
-                            <div class="amount-text">粉丝数</div>
-                            <div class="amount-num">{{fan_people}}</div>
-                        </div>
-                    </div>
+            </div>
+            <div class="choose_content">
+                <img class="choose_content_icon" src="../assets/personal/selfinfo.png">
+                <div class="choose_content_text">
+                    用户信息
+                </div>
+            </div>
+            <div class="choose_content">
+                <img class="choose_content_icon" src="../assets/personal/selflike.png">
+                <div class="choose_content_text">
+                    收藏夹
+                </div>
+            </div>
+            <div class="choose_content">
+                <img class="choose_content_icon" src="../assets/personal/selfsetup.png">
+                <div class="choose_content_text">
+                    设置
                 </div>
             </div>
         </div>
-
-        <!--个人页面-->
-        <div class="user-page">
-
+        <div class="writeframe">
+            <div class="writeimg">
+                <img class="200px" src="../assets/personal/writepassage.png">
+            </div>
+            <div class="writesth">
+                <img class="writeicon" src="../assets/personal/writeicon.png">
+                <div class="writecontent">
+                    添加日志
+                </div>
+            </div>
         </div>
     </div>
+      <div class="rightpart">
+          <div class="welcome">
+              <div class="welcome_left">
+                  <div class="welcomeback">
+                      <div class="welcomecontent">
+                          Hi,欢迎回来
+                      </div>
+                      <div class="welcomesb">
+                          {{sbname}}
+                      </div>
+                      <div class="welcome_right">
+                          <img class="welimg" src="../assets/personal/welimg.png">
+                      </div>
+                  </div>
+                  <div class="folandfans">
+                      <img class="fol" src="../assets/personal/follow.png">
+                      <div class="fcontent">
+                          <div class="fconts">
+                              关注数
+                          </div>
+                          <div class="fconts" style="margin-top: 6px">
+                              238
+                          </div>
+                      </div>
+                      <img class="fans" src="../assets/personal/fans.png">
+                      <div class="fcontent">
+                          <div class="fconts">
+                              粉丝数
+                          </div>
+                          <div class="fconts" style="margin-top: 6px">
+                              262
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="myrecord">
+
+          </div>
+      </div>
   </div>
 
 </template>
@@ -74,10 +98,11 @@ export default {
   },
   data(){
       return{
+          sbname:"账户1234",
           vip_grade:1,
-          opt:[
-              {id:0,name:"主页"},
-              {id:1,name:"个人资料"},
+          contents:[
+              {id:0,name:"个人主页"},
+              {id:1,name:"用户信息"},
               {id:2,name:"收藏夹"},
               {id:3,name:"设置"},
           ],
@@ -87,9 +112,9 @@ export default {
       }
   },
   methods:{
-      handleItemClick(id) {
+      chooseinone() {
           // 处理选项点击事件，切换选项的激活状态
-          this.chooseId = id;
+
           // 在这里可以执行其他相应的操作，根据选项的 id 来处理不同的逻辑
       },
   }
@@ -101,93 +126,232 @@ export default {
     position: absolute;
     display: flex;
     width: 100vw;
-    height: 100vh;
-    justify-content: center;
+    height: auto;
 
-    background-color: #D9D9D9;
+    background-color: #F1F3FF;
 }
-.main-content{
+
+.left_choose{
     position: relative;
     display: flex;
     flex-direction: column;
-    top:80px;
-    width: 1480px;
-    height: 800px;
+    width: 227px;
+    height: 834px;
+    margin-top: 80px;
 }
-/* 上部区 */
-.up-content{
+
+.choosein{
+    position: absolute;
+    width: 227px;
+    height: 86px;
+    flex-shrink: 0;
+    border-radius: 20px;
+    background: #FFF;
+    backdrop-filter: blur(2px);
+}
+
+.left_choose_change{
     position: relative;
     display: flex;
     flex-direction: column;
-    //top:80px;
-    width: 1480px;
-    height: 200px;
+    width: 100%;
+    height: 384px;
+    margin-top: 23px;
+    margin-left: 25px;
 }
-/* 上部用户区 */
-.user-content{
+
+.choose_content{
     position: relative;
     display: flex;
     width: 100%;
-    height: 140px;
-    background-color: #E4E8FC;
-}
-.user-head-image{
-    position: relative;
-    width: 50px;
-    height: 50px;
-    margin-left: 40px;
-    margin-top: 65px;
-}
-.user-name-text{
-    position: relative;
-    display: flex;
-    width: max-content;
-    height: 30px;
-    margin-left: 14px;
-    margin-top: 78px;
+    height: 86px;
+    border-radius: 20px;
+    margin-top: 10px;
+    background-color: #F1F3FF;
+    justify-content: flex-start;
+    align-items: center;
 
-    color: #4C4C4C;
+    color: #8097FD;
     font-family: MiSans;
-    font-size: 18px;
+    font-size: 24px;
     font-style: normal;
-    font-weight: 400;
+    font-weight: 700;
     line-height: normal;
 }
-.vip-grade{
+
+.choose_content_icon{
     position: relative;
     display: flex;
-    width: max-content;
-    height: 15px;
-    margin-left: 4px;
-    margin-top: 70px;
-    border-radius: 6.5px;
-    background-color: #8097FD;
+    width:40px;
+    height: 40px;
+    margin-left: 23px;
+}
+
+.choose_content_text{
+    position: relative;
+    display: flex;
+    width:max-content;
+    height: 30px;
+    margin-left: 37px;
+
+    color: #8097FD;
+    font-family: MiSans;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.writeframe{
+    width: 227px;
+    height: 268px;
+    flex-shrink: 0;
+    margin-top: 128px;
+}
+
+.writesth{
+    width: 227px;
+    height: 68px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border-radius: 20px;
+    background: #D9DFFD;
+    margin-left: 30px;
+}
+
+.writeimg{
+    position: relative;
+    display: flex;
+    width: 200px;
+    height: 200px;
+    margin-left: 40px;
+}
+
+.writeicon{
+    position: relative;
+    display: flex;
+    width: 35px;
+    height: 35px;
+    margin-left: 48px;
+}
+
+.writecontent{
+    position: relative;
+    display: flex;
+    width:max-content;
+    height: 26px;
+    margin-left: 10px;
 
     color: #FFF;
-    font-family: Microsoft New Tai Lue;
-    font-size: 8px;
+    font-family: MiSans;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.rightpart{
+    position: relative;
+    width:max-content;
+    height: max-content;
+    overflow-y: auto;
+    padding: 10px;
+    box-sizing: border-box;
+    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+}
+
+.welcome{
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    width: max-content;
+    height: 585px;
+    margin-left: 22px;
+    margin-top: 150px;
+}
+
+.welcome_left{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width:max-content;
+    height: 355px;
+    border-radius: 40px;
+    background: #D9DFFD;
+    margin-top: 50px;
+}
+
+.welcomeback{
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width:max-content;
+    height: 50px;
+    margin-top: 60px;
+    margin-left: 69px;
+}
+
+.welcomecontent{
+    position: relative;
+    display: flex;
+    width: 194px;
+    height: 41px;
+
+    color: #000;
+    font-family: MiSans;
+    font-size: 32px;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
-    letter-spacing: 1.98px;
 }
-/* 上部切换区 */
-.borad{
-    position: relative;
-    display: flex;
-    width: 100%;
-    height: 60px;
-    background-color: #FFFFFF;
-}
-.choose{
-    position: relative;
-    display: flex;
-    width: 150px;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-    background-color: #FFFFFF;
 
+.welcomesb{
+    position: relative;
+    display: flex;
+    width: 166px;
+    height: 50px;
+    margin-left: 20px;
+    align-items: center;
+    justify-content: center;
+
+    border-radius: 10px;
+    background: #8097FD;
+
+    color: #FFF;
+    font-family: MiSans;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.folandfans{
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    width: 322px;
+    height: 57px;
+    align-items: center;
+    gap: 38px;
+    margin-top: 96px;
+    margin-left: 69px;
+}
+
+.fcontent{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 54px;
+    height: 57px;
+}
+
+.fconts{
+    position: relative;
     color: #888;
     font-family: MiSans;
     font-size: 18px;
@@ -195,66 +359,25 @@ export default {
     font-weight: 400;
     line-height: normal;
 }
-.choose_active{
-    background-color: #8097FD;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.05) inset;
-    color:#FFFFFF;
-}
-.special-user{
-    position: relative;
-    display: flex;
-    justify-content: flex-end;
-    width: 880px;
-    height: 100%;
-}
-.special-user-board{
-    position: relative;
-    display: flex;
-    width: 115px;
-    height: 52px;
-    margin-top: 8px;
-    margin-right: 45px;
-    align-items: center;
-}
-.special-user-icon{
-    position: relative;
-    width: 45px;
-    height: 45px;
-}
-.special-user-text{
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    width: 62px;
-    height: 50px;
-    margin-left: 4px;
-    justify-content: center;
 
-    color: #888;
-    font-family: MiSans;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-}
-.amount-text{
+.welcome_right{
     position: relative;
-    width: 100%;
-    height: 24px;
+    width: 850px;
+    height: 585px;
+    margin-left: 50px;
 }
-.amount-num{
-    position: relative;
-    width: 100%;
-    height: 26px;
-}
-/* 个人资料部分 */
-.user-page{
-    position: relative;
-    display: flex;
-    width: 1480px;
-    height: 600px;
-    margin-top: 50px;
 
-    background-color: #FFFFFF;
+.welimg{
+    position: relative;
+    width: 761px;
+    height: 521px;
+    margin-top: 130px;
 }
+
+.myrecord{
+    position: relative;
+    width: 1528px;
+    height: 798px;
+}
+
 </style>
