c555c57264e172fb37cf781ace7cdce3a8820dd9
huhukxg <kxg61@tongji.edu.cn>
2023-08-20 10:40:01 +0800

    标题修改

--- src/components/Attraction/smallfunc/startline.vue
+++ src/components/Attraction/smallfunc/startline.vue
@@ -1,40 +1,53 @@
 <template>
     <div class="linebox">
         <div class="line"></div>
-        <div class="linetitle">{{ title }}</div>
+        <div class="linetitle">
+            <div>{{ title }}</div>
+            <div class="enlinetitle">{{ entitle }}</div>
+        </div>
         <div class="line"></div>
     </div>
+
+   
+    
 </template>
 
 <script>
 export default {
     props: {
-        title: String
+        title:String,
+        entitle:String
     }
 }
 </script>
 
 <style>
+
 .linebox {
-  display: flex;
-  flex-direction: row;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  width: 70%;
-  margin: 20px auto 5px auto; /* Center the linebox on the screen */
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    margin: 0 auto 5px auto;
+    padding-top: 10px;
+    
 }
 
 .linetitle {
     flex-wrap: nowrap;
-    font-size: 22px;
+    font-size: 20px;
     font-weight: bold;
-
+    color: #8097FD;
 }
-
+.enlinetitle{
+    margin-top: 5px;
+    font-size: 10px;
+    color: #999;
+}
 .line {
     flex-grow: 1;
-    border-bottom: 1px #c2c2c2 solid;
+    border-bottom: 1px #cdcdcd solid;
     margin: 15px;
-}
-</style>
+}</style>
